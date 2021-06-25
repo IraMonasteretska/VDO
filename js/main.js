5d16a5f259ffcf8f2a586e9fb665b5a42900b64b
BENDeR-BK <bogdanKvasov@gmail.com>
2021-06-25 12:46:50 +0300

    pravky

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
     $('.form_select').niceSelect();
 
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+          $("header").addClass("header_fixed");
+        } else {
+          $("header").removeClass("header_fixed");
+        }
+      });
+
 
     $('[data-trigger]').on('click', function(){
         let trigger = $(this).data('trigger')
@@ -27,80 +35,90 @@ $(document).ready(function() {
     // comment Slider /////////////
     ///////////////////////////////
     
-        if (window.matchMedia('(max-width: 767px)').matches) {
-            
-            var $reviewsSlider = $(".comment__slider");
-           
-            if ($reviewsSlider.length) {
-                $reviewsSlider.not('.slick-initialized').slick({
-                    infinite: true,
-                    autoplaySpeed: 5000,
-                    // autoplay: true,
-                    slidesToShow: 2,
-                    // adaptiveHeight: true,
-                    slidesToScroll: 2,
-                    swipeToSlide: true,
-                    dots: true,
-                    nextArrow: $(".comment__slider-next"),
-                    prevArrow: $(".comment__slider-prev"),
-                    responsive: [
-                        {
-                            breakpoint: 576,
-                            settings: {
-                                slidesToShow: 1,
-                            },
-                        },
-                        {
-                            breakpoint: 370,
-                            settings: {
-                                slidesToShow: 1,
-                            },
-                        },
-                    ],
-                });
-            
-            }
-            
-        } 
-        $(window).resize(function(){
-            if($(window).width() < 767){
-                // var $reviewsSlider = $(".comment__slider");
-                $(".comment__slider").not('.slick-initialized').slick({
-                    infinite: true,
-                    autoplaySpeed: 5000,
-                    // autoplay: true,
-                    slidesToShow: 2,
-                    // adaptiveHeight: true,
-                    slidesToScroll: 2,
-                    swipeToSlide: true,
-                    dots: true,
-                    nextArrow: $(".comment__slider-next"),
-                    prevArrow: $(".comment__slider-prev"),
-                    responsive: [
-                        {
-                            breakpoint: 576,
-                            settings: {
-                                slidesToShow: 1,
-                            },
-                        },
-                        {
-                            breakpoint: 370,
-                            settings: {
-                                slidesToShow: 1,
-                            },
-                        },
-                    ],
-                });
-            } else {
-                if ($(".comment__slider.slick-initialized").length) {
-                    $(".comment__slider").slick('unslick');
-                }
-            }
-
-        })
+    if (window.matchMedia('(max-width: 767px)').matches) {
         
+        var $reviewsSlider = $(".comment__slider");
+        
+        if ($reviewsSlider.length) {
+            $reviewsSlider.not('.slick-initialized').slick({
+                infinite: true,
+                autoplaySpeed: 5000,
+                // autoplay: true,
+                slidesToShow: 2,
+                // adaptiveHeight: true,
+                slidesToScroll: 2,
+                swipeToSlide: true,
+                dots: true,
+                nextArrow: $(".comment__slider-next"),
+                prevArrow: $(".comment__slider-prev"),
+                responsive: [
+                    {
+                        breakpoint: 576,
+                        settings: {
+                            slidesToShow: 1,
+                        },
+                    },
+                    {
+                        breakpoint: 370,
+                        settings: {
+                            slidesToShow: 1,
+                        },
+                    },
+                ],
+            });
+        
+        }
+        
+    } 
+    $(window).resize(function(){
+        if($(window).width() < 767){
+            // var $reviewsSlider = $(".comment__slider");
+            $(".comment__slider").not('.slick-initialized').slick({
+                infinite: true,
+                autoplaySpeed: 5000,
+                // autoplay: true,
+                slidesToShow: 2,
+                // adaptiveHeight: true,
+                slidesToScroll: 2,
+                swipeToSlide: true,
+                dots: true,
+                nextArrow: $(".comment__slider-next"),
+                prevArrow: $(".comment__slider-prev"),
+                responsive: [
+                    {
+                        breakpoint: 576,
+                        settings: {
+                            slidesToShow: 1,
+                        },
+                    },
+                    {
+                        breakpoint: 370,
+                        settings: {
+                            slidesToShow: 1,
+                        },
+                    },
+                ],
+            });
+        } else {
+            if ($(".comment__slider.slick-initialized").length) {
+                $(".comment__slider").slick('unslick');
+            }
+        }
 
-
+    })
+        
     // end comment Slider
+
+    // scroll to section
+  if (document.querySelector(".scroll-section")) {
+    $(".scroll-section").on("click", function (event) {
+      event.preventDefault();
+      $(".scroll-section").removeClass("active");
+      $(this).addClass("active");
+      var id = $(this).attr("href"),
+        top = $(id).offset().top - 200;
+      $("body,html").animate({ scrollTop: top }, 1000);
+    });
+  }
 
 });
